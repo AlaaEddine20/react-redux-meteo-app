fef04b415527bd9bae653eb358e80935e0a0744b
Alaa Eddine <keni91cell@gmail.com>
2022-02-25 18:08:30 +0100

    adds style and data to details page

--- src/components/CityCard/CityCard.js
+++ src/components/CityCard/CityCard.js
@@ -4,12 +4,21 @@ import moment from "moment";
 import "./styles/index.scss";
 
 const CityCard = ({ cityName, temperature, weather, icon }) => {
+
+  const giveClassByWeatherType = () => {
+    if (weather == "Clouds") {
+      return "clouds";
+    } else if (weather == "Clear") {
+      return "clear";
+    } else if (weather == "Rain") {
+      return "rain";
+    } else if (weather == "Snow") {
+      return "snow";
+    }
+  };
+
   return (
-    <div
-      className={`cityCard ${weather === "Clouds" && "clouds"} ${
-        weather === "Clear" && "clear"
-      } ${weather === "Rain" && "rain"} ${weather === "Snow" && "snow"}`}
-    >
+    <div className={`cityCard ${giveClassByWeatherType()}`}>
       <div className="left-content">
         {cityName && <p className="city-name">{cityName}</p>}
         <p className="date">{moment().format("dddd, DD, MMMM ")}</p>
@@ -19,7 +28,7 @@ const CityCard = ({ cityName, temperature, weather, icon }) => {
         <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="" />
       </div>
       <div className="right-content">
-        <p className="temperature">{temperature}</p>
+        <p className="temperature">{temperature.toFixed(0)}</p>
       </div>
     </div>
   );
--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -2,6 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
+import moment from "moment";
+
+import IconBack from "../../assets/Arrow - Left.png";
+import AddButton from "../../assets/Plus-white.png";
+
+import "./styles/index.scss";
+
 const DetailPage = () => {
   const allLocations = useSelector((state) => state.weatherReducer.locations);
 
@@ -10,7 +17,43 @@ const DetailPage = () => {
   const currentItem =
     allLocations.length > 0 ? allLocations.find((item) => id == item.id) : {};
 
-  return <div>{currentItem.name}</div>;
+  const weather = currentItem.weather[0].main || "";
+  const temperature = currentItem.main.temp || null;
+  const weatherIcon = currentItem.weather[0].icon || "";
+
+  const giveClassByWeatherType = () => {
+    if (weather == "Clouds") {
+      return "clouds";
+    } else if (weather == "Clear") {
+      return "clear";
+    } else if (weather == "Rain") {
+      return "rain";
+    } else if (weather == "Snow") {
+      return "snow";
+    }
+  };
+
+  return (
+    <div className={`weather-city-details ${giveClassByWeatherType()}`}>
+      <div className="header">
+        <img src={IconBack} alt="icon back" className="icon-back"></img>
+        <h1 className="city-name">{currentItem.name}</h1>
+        <img src={AddButton} alt="add button" className="icon-add"></img>
+      </div>
+      <div className="hero-section">
+        <h2 className="date">{moment().format("dddd, DD, MMMM ")}</h2>
+        <h3 className="weather-type">{weather}</h3>
+        <div className="hero-subSection">
+          <img
+            src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
+            alt="weather icon"
+            className="weather-icon"
+          />
+          <h6 className="temperature">{temperature.toFixed(0)}</h6>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailPage;
